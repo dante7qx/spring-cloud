1eaab2b5cf8b2b53a82d0e5bb87f68fbecb8fc70
dante <sunchao.0129@163.com>
2017-01-24 13:13:54 +0800

    修改配置文件

--- demo-sysmgr-rest/src/test/java/org/dante/demo/sysmgr/DemoSysmgrRestApplicationTests.java
+++ demo-sysmgr-rest/src/test/java/org/dante/demo/sysmgr/DemoSysmgrRestApplicationTests.java
@@ -2,15 +2,24 @@ package org.dante.demo.sysmgr;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.netflix.discovery.DiscoveryClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoSysmgrRestApplicationTests {
+	
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
 	@Test
-	public void contextLoads() {
+	public void shutdown() {
+//		DiscoveryManager.getInstance().shutdownComponent();
 	}
+	
+	
 
 }
